1b53eb6de7aca871ce646dd05c8b0031c543d2ca
semyon422 <semyon422@users.noreply.github.com>
2023-05-02 22:36:31 +0500

    Refactor main.lua

--- main.lua
+++ main.lua
@@ -4,10 +4,6 @@ print(test.test(1, 2))
 local ffi = require("ffi")
 ffi.cdef("double add(double a, double b);")
 
-local libtest
-if jit.os == "Windows" then
-    libtest = ffi.load("libtest")
-else
-    libtest = ffi.load("test")
-end
+local libprefix = jit.os == "Windows" and "lib" or ""
+local libtest = ffi.load(libprefix .. "test")
 print(libtest.add(1, 2))
